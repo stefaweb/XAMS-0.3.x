35981080ccd97e431084d7e046b246229787a3b3
stefaweb <sleclerc@actionweb.fr>
2017-02-11 14:27:47 +0100

    Update gui/include/dns.php

--- gui/include/dns.php
+++ gui/include/dns.php
@@ -54,8 +54,8 @@ class dns extends xclass
                     }
                 }
             }
+            while (true);
         }
-        while (true);
     }
 
     public function multiply()
